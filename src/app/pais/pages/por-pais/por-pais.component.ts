341e8d785ee1fae3e396fcfed864915b53ea3bcd
Fredy Rivera <fredyjesus2013@gmail.com>
2021-03-09 19:11:53 -0500

    Manejo de errores sin rxjs

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,14 +10,18 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
 
   constructor( private paisService: PaisService) { }
 
   buscar(): void {
+    this.hayError = false;
     if ( this.termino.trim().length > 0 ){
       this.paisService.BuscarPais( this.termino )
           .subscribe( resp => {
             console.log( resp );
+          }, (err) => {
+            this.hayError = true;
           });
     }
   }
